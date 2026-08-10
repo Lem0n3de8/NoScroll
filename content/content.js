b21f075729a6e5a0dc19d653909aefa150c30c78
Lem0n3de8 <lem0n3de8@gmail.com>
2026-08-11 00:04:31 +0200

    Fix: Complete grayscale overhaul

--- content/content.js
+++ content/content.js
@@ -84,11 +84,35 @@ function setVoidMode(enabled){
 }
 
 function setGrayScale(enabled){
-    //Grayscale on home page
-    const posts = document.querySelectorAll("article");
-    for (const post of posts){
-        console.log("Applying grayscale");
-        post.classList.toggle("apply-grayscale", enabled);
+    // img -> profile pictures, image posts 
+    const imgs = document.querySelectorAll("img");
+    for (const img of imgs){
+        img.classList.toggle("apply-grayscale", enabled);
+    }
+    // video -> video posts
+    const videos = document.querySelectorAll("video");
+    for (const video of videos){
+        video.classList.toggle("apply-grayscale", enabled);
+    }
+    // button -> image post page button
+    const buttons = document.querySelectorAll("button");
+    for (const button of buttons){
+        button.classList.toggle("apply-grayscale", enabled);
+    }
+    // svg -> repost blue icon
+    const svgs = document.querySelectorAll("svg");
+    for (const svg of svgs){
+        svg.classList.toggle("apply-grayscale", enabled);
+    }
+    // a -> hashtags
+    const as = document.querySelectorAll("a");
+    for (const a of as){
+        a.classList.toggle("apply-grayscale", enabled);
+    }
+    // canva -> story rainbow/green circle
+    const canvas = document.querySelectorAll("canvas");
+    for (const canva of canvas){
+        canva.classList.toggle("apply-grayscale", enabled);
     }
 }
 
