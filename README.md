ae723eaeddc57c98d41f4a7fc11886412b56f536
Lem0n3de8 <lem0n3de8@gmail.com>
2026-08-30 22:51:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# NoScroll
+
+A Firefox extension that removes Instagram's most distracting features while keeping messaging intact.
+    
+[![Static Badge](https://img.shields.io/badge/Firefox_add--on-blue?style=for-the-badge&logo=firefox)](https://addons.mozilla.org/en-US/firefox/addon/noscrollinstagram/)
+[![Static Badge](https://img.shields.io/badge/Maintained%3F-yes-brightgreen?style=for-the-badge)](https://github.com/Lem0n3de8/NoScroll/commits)
+[![Static Badge](https://img.shields.io/badge/License-MIT-green?style=for-the-badge)](./LICENSE)
+[![Static Badge](https://img.shields.io/badge/Latest--version-0.5.0-blue?style=for-the-badge)](https://github.com/Lem0n3de8/NoScroll/releases)
+      
+## Table of Contents
+
++ [About](#about)
++ [How to install](#how-to-install)
++ [Current features](#current-features)
++ [Currently supported browsers](#currently-supported-browsers)
++ [Privacy Notice](#privacy-notice)
++ [Help development](#help-development)
+
+
+## About
+Every time I open Instagram just to reply to a friend, I eventually end up scrolling. NoScroll was created to remove the most distracting parts of Instagram while keeping messaging and other essential features accessible.
+
+NoScroll is a content blocker for Instagram available for Firefox. With a simple popup UI, you can toggle on/off distracting elements such as Stories, Reels and Explore page.
+
+## How to Install
+
+- Install NoScroll like any other extension via Firefox's addons website. You can find the extension here: [NoScroll for Firefox](https://addons.mozilla.org/en-US/firefox/addon/noscrollinstagram/)
+- Alternatively you can download the latest version from the `Releases` tab: [NoScroll for Firefox](https://github.com/Lem0n3de8/NoScroll/releases)
+    - To install web extensions from file, go to `about:addons` and select `Install add-on from file` in the gear icon at the top right.
+
+## Current Features:
+Contains a simple popup user interface to toggle settings:
+
+<img src="misc/images/ui_preview_0.5.0.png" width="300">
+
+| Setting                  | Description |
+| ------------------------ | ------------------------ |
+| Sidebar reels icon       | Hide the reel icon from the left sidebar |
+| Home page stories        | Hide the stories at the top of the Home page |
+| Redirect `/reels` page   | Redirect any URL containing `instagram/reels` to `instagram.com` |
+| Home page feed | Hide the posts and reels suggested in the home page |
+| Apply grayscale | Add grayscale covering the whole website |
+| Void mode                | Turn the page blank |
+
+There are also some contents that are removed by default: 
+- Explore page posts
+
+## Currently Supported Browsers
+
+**Officially Supported:**
+- <img src="misc/images/firefox_logo.png" width="12" > Firefox
+- <img src="misc/images/zen_logo.png" width="12" > Zen browser
+
+Other Firefox-based browsers may work but have not been tested.
+
+## Privacy Notice
+NoScroll is simple and privacy friendly. It does not collect any data or require special permissions.
+
+## Help Development
+
+### Pull Requests
+
+Please open an issue before starting work on a feature or significant change.
+
+All pull requests should target the `develop` branch rather than `main`.
+
+Suggested Improvements:
+- Test the extension on other browsers
+- Improve popup CSS
+
+### Submit an Issue
+To submit an issue, check the [issues tab](https://github.com/Lem0n3de8/NoScroll/issues).
+
+----
+
+**Early development** — features and behavior may change.
---
+++ content/content.js
@@ -0,0 +1,169 @@
+const CONFIG = {
+    instagramUrl: "https://www.instagram.com/",
+    selectors:{
+        homePageStories: '[data-pagelet="story_tray"]',
+        reelsTab: 'a[href="/reels/"]',
+        exploreTab: 'a[href="/explore/"]',
+        loadingState: '[data-visualcompletion="loading-state"]'
+    }
+}
+
+
+function hideHomeFeed(hidden){
+    const posts = document.querySelectorAll("article");
+    const loadingWheel = document.querySelector(CONFIG.selectors.loadingState);
+    
+    for (const post of posts){
+        post.classList.toggle("hidden-by-extension", hidden);
+    }
+    // Remove the loading wheel
+    if (loadingWheel) loadingWheel.classList.toggle("hidden-by-extension", hidden);
+    
+    // Disable scrolling 
+    if (hidden) {
+        document.body.style.setProperty("overflow", "hidden", "important");
+    } else {
+        document.body.style.removeProperty("overflow");
+    }
+}
+
+function blockReelsPage(hidden ) {
+  if (hidden && window.location.href.includes('instagram.com/reels')) {
+    window.location.href = CONFIG.instagramUrl;
+  }
+}
+
+function setStoriesHidden(hidden) {
+    const stories = document.querySelector(CONFIG.selectors.homePageStories);
+    if (!stories) return;
+
+    stories.classList.toggle("hidden-by-extension", hidden);
+}
+
+function setReelsTabHidden(hidden){
+    const reelsTab = document.querySelector(CONFIG.selectors.reelsTab);
+    if (!reelsTab) return;
+
+    reelsTab.classList.toggle("hidden-by-extension", hidden);
+}
+
+function setVoidMode(enabled){
+    const STYLE_ID = "instagram-void-mode";
+
+    let style = document.getElementById(STYLE_ID);
+
+    if (enabled) {
+        if (!style) {
+            style = document.createElement("style");
+            style.id = STYLE_ID;
+            style.textContent = `
+                * {
+                    display: none !important;
+                }`
+                
+                ;
+            document.head.appendChild(style);
+        }
+    } else {
+        style?.remove();
+    }
+
+}
+
+function setGrayScale(enabled){
+    // img -> profile pictures, image posts 
+    const imgs = document.querySelectorAll("img");
+    for (const img of imgs){
+        img.classList.toggle("apply-grayscale", enabled);
+    }
+    // video -> video posts
+    const videos = document.querySelectorAll("video");
+    for (const video of videos){
+        video.classList.toggle("apply-grayscale", enabled);
+    }
+    // button -> image post page button
+    const buttons = document.querySelectorAll("button");
+    for (const button of buttons){
+        button.classList.toggle("apply-grayscale", enabled);
+    }
+    // svg -> repost blue icon
+    const svgs = document.querySelectorAll("svg");
+    for (const svg of svgs){
+        svg.classList.toggle("apply-grayscale", enabled);
+    }
+    // a -> hashtags
+    const as = document.querySelectorAll("a");
+    for (const a of as){
+        a.classList.toggle("apply-grayscale", enabled);
+    }
+    // canvas -> story rainbow/green circle
+    const canvases = document.querySelectorAll("canvas");
+    for (const canvas of canvases){
+        canvas.classList.toggle("apply-grayscale", enabled);
+    }
+}
+
+function isExplorePage(){
+    return location.pathname === "/explore/";
+}
+
+function updateExplorePage(onExplorePage){
+    if (!onExplorePage){
+        
+        try{
+            const loadingWheel = document.querySelector(CONFIG.selectors.loadingState);
+            if (loadingWheel) loadingWheel.classList.remove("hidden-by-extension");
+        }
+        catch{
+            return
+        }
+    }
+    else{
+        const anchors = document.querySelectorAll("a");
+        const loadingWheel = document.querySelector(CONFIG.selectors.loadingState);
+
+        for (const anchor of anchors){
+            if (anchor.href.includes("/p/")){
+                anchor.classList.add("hidden-by-extension");
+            } 
+        }
+
+        if (loadingWheel) loadingWheel.classList.add("hidden-by-extension");
+    }
+}
+
+async function applySettings() {
+    const settings = await browser.storage.local.get();
+    
+    const path = window.location.pathname;
+    
+    // Home page settings
+    if (window.location.href === CONFIG.instagramUrl) {
+        setStoriesHidden(settings.homeStories ?? false);
+        hideHomeFeed(settings.homeFeed ?? false);
+    }
+
+    // General settings
+    setVoidMode(settings.voidMode ?? false);
+    setReelsTabHidden(settings.sideReels ?? false);
+    blockReelsPage(settings.redirectReels ?? false);
+    setGrayScale(settings.grayScale ?? false);
+
+    // auto enabled settings
+
+    updateExplorePage(isExplorePage());
+}
+
+browser.storage.local.onChanged.addListener((changes) =>{
+    console.log("Detected changes", changes);
+    applySettings();
+})
+
+const observer = new MutationObserver(() => {
+    applySettings();
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
